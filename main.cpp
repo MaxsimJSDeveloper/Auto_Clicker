0fe6663bac22d522254ea9d2a0e2d5071f4e97ec
Holovko Maksym <golovkomaksim852@gmail.com>
2026-02-13 09:52:49 +0200

    feat: add randomized click delay and custom speed input

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <ctime>
 #include <windows.h>
 
 using namespace std;
 
 int main() {
+    srand(time(0));
+
+    int speed;
     cout << "=== AutoClicker ===\n";
+	cout << "Shose the speed of the clicker:\n";
+    cin >> speed;
+
+    if(speed <= 0) {
+        cout << "Speed must be a positive" << endl;
+        return 1;
+	}
+
+    if (speed > 1000)
+    {
+        cout << "Speed cannot be more then 1000" << endl;
+        return 1;
+    }
+
     cout << "Press 'F8' to START/STOP\n";
-    cout << "Press 'END' to EXIT\n\n";
+    cout << "Press 'ESC' to EXIT";
 
     bool active = false;
 
@@ -23,7 +41,8 @@ int main() {
             mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, 0);
             mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, 0);
 
-            Sleep(100);
+            int finalDelay = speed + (rand() % 31);
+            Sleep(finalDelay);
         }
     }
 
